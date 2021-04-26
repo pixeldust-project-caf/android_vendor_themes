b70e2329baf3424e98545daffad494d3b8be1e66
Advaith Bhat <advaithbhat9@gmail.com>
2021-04-26 08:57:48 +0000

    Themes: Introduce Custom Statusbar Signal & Wi-Fi icons
    - Thanks to sis, acherus, iconicicons & cool_modules_69 for these beautiful icons.
    
    AEX Edits:
    
    - move current overlays from Icons to IconPacks.
    - Android.mk > Android.bp
    
    Change-Id: Id9e57869f75b21f27322c4472f0902c421a25297
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- icons.mk
+++ icons.mk
@@ -50,3 +50,22 @@ PRODUCT_PACKAGES += \
     IconShapeTeardropOverlay \
     IconShapeVesselOverlay \
     IconShapeRoundedHexagonOverlay
+
+# Signal / WiFi Icons
+PRODUCT_PACKAGES += \
+    BarsSignalOverlay \
+    BarsWiFiOverlay \
+    InsideSignalOverlay \
+    InsideWiFiOverlay \
+    RoundSignalOverlay \
+    RoundWiFiOverlay \
+    SneakySignalOverlay \
+    SneakyWiFiOverlay \
+    StrokeSignalOverlay \
+    StrokeWiFiOverlay \
+    WavySignalOverlay \
+    WavyWiFiOverlay \
+    XperiaSignalOverlay \
+    XperiaWiFiOverlay \
+    ZigZagSignalOverlay \
+    ZigZagWiFiOverlay
