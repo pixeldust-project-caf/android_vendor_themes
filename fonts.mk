4354e2248054284ef9dcb78f8b9c683debafb515
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-12-25 10:07:47 +0100

    fonts: Redo custom fonts
    * Everything is migrated to blueprint
    * drop unused shit in twelve
    * drop all existing fonts and re-add the ones we want
    * switch from fonts.xml to fonts_customization.xml
    * included fonts are Google Sans, Inter v3.19, Linotte,
      Manrope v4.504, OnePlus Sans and OnePlus Slate
    
    Co-authored-by: Jyotiraditya Panda <jyotiraditya@aospa.co>
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: idoybh <idoybh2@gmail.com>
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

---
+++ fonts.mk
@@ -0,0 +1,12 @@
+# Fonts
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,vendor/themes/fonts//prebuilt,$(TARGET_COPY_OUT_PRODUCT)/fonts) \
+    vendor/themes/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
+
+PRODUCT_PACKAGES += \
+    FontGoogleSansOverlay \
+    FontInterOverlay \
+    FontLinotteSourceOverlay \
+    FontManropeOverlay \
+    FontOnePlusSansOverlay \
+    FontOneplusSlateSourceOverlay
