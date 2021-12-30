256729e167ef3d6935802316f88df32024a619b9
Alex <du.alexcruz@gmail.com>
2021-12-30 20:04:31 +0100

    Navbar: Introduce navbar styles [3/3] Change-Id: I5221e26bdd13d8e8c072d42bae0bd4b6833a05af
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- navbar.mk
+++ navbar.mk
@@ -1,3 +1,15 @@
 # Gestural Navigation Bar Fullscreen
 PRODUCT_PACKAGES += \
      NavigationBarModeGesturalOverlayFS
+
+# Themes
+PRODUCT_PACKAGES += \
+    NavbarAsusOverlay \
+    NavbarOnePlusOverlay \
+    NavbarOneUiOverlay \
+    NavbarTecnoCamonOverlay \
+    NavbarAndroidOverlay \
+    NavbarMotoOverlay \
+    NavbarNexusOverlay \
+    NavbarOldOverlay \
+    NavbarSammyOverlay
